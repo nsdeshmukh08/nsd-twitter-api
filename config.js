6e013764410dc0dfd651fbdba85b1071415a7f4c
nsdeshmukh08 <nsdeshmukh08@gmail.com>
2021-09-18 12:45:28 +0530

    git config update.

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    consumer_key:         'IvFB5TGHvNTwtp5bDfoCRil4v',
-    consumer_secret:      'zPDwHsSL9T1EOdIuYbqe5qCBUCyGrE399gJoVZxYTgFhSVTJcm',
-    access_token:         '1438813277811662854-pwRYiDZDWB0HrZFxRM4Kn31Uw43IcF',
-    access_token_secret:  'ZqloJpSh17xPF7EVoSTlJutPciTDgb0K0MZlhka1AJYbD',
+    consumer_key:         '',
+    consumer_secret:      '',
+    access_token:         '',
+    access_token_secret:  '',
     timeout_ms:           60*1000, 
 };
